abd1633cd35b0ac684cb4a8ce3e004d7d5fa5aca
Abdelrazek <abdelrazekhdd@gmail.com>
2021-07-30 21:15:07 +0200

    create dynamic navigation

--- js/app.js
+++ js/app.js
@@ -1,57 +1,61 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-
+ *
+ */
+const sections = document.querySelectorAll("section");
+const ul = document.querySelector(".navbar__menu ul");
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
+ *
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
-
+ *
+ */
+let buildNav = () => {
+  sections.forEach((section) => {
+    const li = document.createElement("li");
+    const link = document.createElement("a");
+    link.href = "#" + section.id;
+    link.classList.add("menu__link");
+    link.textContent = section.dataset.nav;
+    li.appendChild(link);
+    ul.appendChild(li);
+  });
+};
 // build the nav
-
 
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
-
+// Build menu
+buildNav();
 // Scroll to section on link click
 
 // Set sections as active
-
-
